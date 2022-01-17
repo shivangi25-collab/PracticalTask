86f33ff4061d3e05908da3f14c416b3fa549697c
shivangi <shivangisavani1997@gmail.com>
2022-01-17 15:37:08 +0530

    Commit

--- lib/apiInterface/provider.dart
+++ lib/apiInterface/provider.dart
@@ -1,18 +1,35 @@
+import 'package:dio/dio.dart';
 import 'package:practical_task/modal/Menu.dart';
 import 'package:http/http.dart' as http;
 
 class ApiProvider{
 
-  static Future<List<MenuResponse>> fetchProducts() async {
-    var response =
-    await http.get(Uri.parse("https://www.mocky.io/v2/5dfccffc310000efc8d2c1ad"));
-    if (response.statusCode == 200) {
-      var data = response.body;
-      print(response.body);
-      return menuFromJson(data);
-    } else {
-      throw Exception();
+  // static Future<List<MenuResponse>> fetchProducts() async {
+  //   var response =
+  //   await http.get(Uri.parse("https://www.mocky.io/v2/5dfccffc310000efc8d2c1ad"));
+  //   if (response.statusCode == 200) {
+  //     var data = response.body;
+  //     print(response.body);
+  //     return menuFromJson(data);
+  //   } else {
+  //     throw Exception();
+  //   }
+  // }
+
+  static Future<List<MenuResponse>?> fetchProducts() async {
+    try{
+      var response =await Dio().get('https://www.mocky.io/v2/5dfccffc310000efc8d2c1ad');
+      if(response.statusCode == 200){
+        print(response);
+        var data = response.data;
+        return menuFromJson(data);
+      }
+    }
+    catch(e){
+      print(e);
+      return null;
     }
   }
+
 
 }
